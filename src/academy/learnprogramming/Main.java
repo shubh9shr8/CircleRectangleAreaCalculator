2fcd1526bf7ed234f9ac81c8468ad3a5665ae937
shubh9shr8 <122762929+shubh9shr8@users.noreply.github.com>
2023-02-05 20:30:33 +0530

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("the area of circle with radius is " + area(5));
+        System.out.println("the area of rectangle with length 2 and breadth is "+ area(2,3));
+    }
+    public static double area (double radius){
+        if (radius < 0) {
+            return -1;
+        }
+        return Math.PI * radius * radius;
+    }
+
+        public static double area(double x, double y){
+        if (x < 0 || y<0){
+    return -1;
+            }
+            return x * y;
+        }
+    }
+
